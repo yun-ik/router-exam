6b35cc45cbf9969041e70366069b03c7397d52eb
yun-ik <jungg123@naver.com>
2021-03-29 16:18:48 +0900

    router-exam1

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import React, { Component } from 'react';
+import Nav from './components/Nav';
+import About from './components/About'
+import Main from './components/Main'
+import Lessons from './components/Lessons'
+import { Route, Switch } from 'react-router';
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route exact path='/' component={Main}></Route>
+          <Route path='/about' component={About}></Route>
+          <Route path='/lessons' component={Lessons}></Route>
+          <Route component={() => <h2>Not Found!</h2>}></Route>
+        </Switch>
+        
+      </div>
+    );
+  } 
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+import{Link, NavLink} from 'react-router-dom';
+
+class Nav extends Component {
+  render(){
+    return (
+      <nav className='navtop'>
+        <h2>Namoosori</h2>
+        <ul className='nav-links'>
+          <li><NavLink exact to='/'>Main</NavLink></li>
+          <li><NavLink exact to='/about'>About</NavLink></li>
+          <li><NavLink exact to='/lessons'>Lessons</NavLink></li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default Nav;
